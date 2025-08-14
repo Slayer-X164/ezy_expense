0199f8b2a7bac93b8486062f58cb30dfacc831fe
Siddhesh <siddheshghag150@gmail.com>
2025-08-14 13:50:41 +0530

    error fix: NextRequest

--- app/api/budgets/[id]/route.ts
+++ app/api/budgets/[id]/route.ts
@@ -1,59 +1,50 @@
 import connectDB from "@/lib/db";
 import Budget from "@/models/budgets.model";
 import Expense from "@/models/expenses.model";
-import { NextResponse } from "next/server";
-import React from "react";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, {params}: { params: { id: string } }) {
-
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
     await connectDB();
-    const id = params.id
+    const id = context.params.id;
     console.log(id);
 
     const budget = await Budget.findById(id);
     if (!budget) {
-      return NextResponse.json({
-        success: false,
-        status: 500,
-      });
+      return NextResponse.json({ success: false, status: 500 });
     }
-    return NextResponse.json({
-      success: true,
-      budget,
-    });
+
+    return NextResponse.json({ success: true, budget });
   } catch (error: any) {
-    console.log("error while fetching budget by ID", error.message);
-    return NextResponse.json({
-      success: false,
-      error: error.message,
-    });
+    console.error("Error while fetching budget by ID:", error.message);
+    return NextResponse.json({ success: false, error: error.message });
   }
 }
 
-export async function DELETE(req: Request, {params}: { params: { id: string } }) {
+export async function DELETE(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
   try {
     await connectDB();
-    const id = params.id;
+    const id = context.params.id;
 
     const budget = await Budget.findByIdAndDelete(id);
-    const expenses = await Expense.deleteMany({ budgetId: id });
+    await Expense.deleteMany({ budgetId: id });
 
     if (!budget) {
-      return NextResponse.json({
-        success: false,
-        status: 500,
-      });
+      return NextResponse.json({ success: false, status: 500 });
     }
+
     return NextResponse.json({
       success: true,
       message: "Budget deleted successfully",
     });
   } catch (error: any) {
-    console.log("error while deleting budget by ID", error.message);
-    return NextResponse.json({
-      success: false,
-      error: error.message,
-    });
+    console.error("Error while deleting budget by ID:", error.message);
+    return NextResponse.json({ success: false, error: error.message });
   }
 }
