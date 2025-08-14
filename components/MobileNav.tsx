efb78d73a3ed5baad3405564661b71717a472df5
Siddhesh <siddheshghag150@gmail.com>
2025-08-14 16:24:10 +0530

    feat: mobile nav complete

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import {motion} from 'framer-motion'
-const MobileNav = ({ openMobileNav }: any) => {
+import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation"
+import { MdDashboard, MdWorkspacePremium } from "react-icons/md"
+import { GrMoney } from "react-icons/gr"
+import { GiPayMoney } from "react-icons/gi"
+
+const links = [
+  { href: "/dashboard/dashboard-page", label: "Dashboard", icon: <MdDashboard className="text-2xl"  />,color:'text-blue-400' },
+  { href: "/dashboard/budgets", label: "Budgets", icon: <GrMoney className="text-2xl" />,color:'text-green-700' },
+  { href: "/dashboard/expenses", label: "Expenses", icon: <GiPayMoney className="text-2xl" />,color:'text-red-400' },
+
+]
+const MobileNav = ({ openMobileNav, setOpenMobileNav }: any) => {
+    const pathname = usePathname()
   useEffect(() => {
     if (openMobileNav) {
       document.body.style.overflow = "hidden"; // disable scroll
@@ -10,23 +22,47 @@ const MobileNav = ({ openMobileNav }: any) => {
       document.body.style.overflow = ""; // restore scroll
     }
     return () => {
-      document.body.style.overflow = ""; // cleanup on unmount
+      document.body.style.overflow = ""; // cleanup
     };
   }, [openMobileNav]);
+
+  const handleClose = () => setOpenMobileNav(false);
+  const {data:session} = useSession()
   return (
     <motion.div
-     initial={{ x: '100%' }}
-      animate={{ x: 0 }}    
-      exit={{ x: '100%' }}
-      transition={{ type: 'tween', duration: 0.3 }}
-     className="bg-black text-neutral-200 h-screen  w-screen absolute  top-[60px] left-0 ">
-
-       <div className="w-full h-[60%] flex items-left p-6 justify-center flex-col text-4xl gap-6 font-bold">
-          <Link href="" className="w-full border-b pb-2">Home</Link>
-          <Link href="" className="w-full border-b pb-2">Features</Link>
-          <Link href="" className="w-full border-b pb-2">Testimonials</Link>
-          <Link href="" className="w-full border-b pb-2">Dashboard</Link>
-        </div>
+      initial={{ x: "100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100%" }}
+      transition={{ type: "tween", duration: 0.3 }}
+      className="bg-black text-neutral-200 h-screen w-screen absolute z-50 top-[60px] left-0"
+    >
+      <div className="w-full h-full flex items-left p-6 justify-self-start flex-col  gap-6 font-bold">
+        <Link href="/" onClick={handleClose} className="w-full border-b pb-2">
+          Home
+        </Link>
+        <Link href="/features" onClick={handleClose} className="w-full border-b pb-2">
+          Features
+        </Link>
+        <Link href="/testimonials" onClick={handleClose} className="w-full border-b pb-2">
+          Testimonials
+        </Link>
+         {links.map((link) => {
+          const isActive = pathname === link.href || pathname.startsWith(link.href + "/")
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              onClick={handleClose}
+              className={`flex items-center gap-2 py-2 px-4 md:px-12 text-2xl ${
+                isActive ? `bg-neutral-800/80 ` : "bg-transparent"
+              }`}
+            >
+              <h3 className={`${link.color}`}>{link.icon}</h3>
+              <h3 className="">{link.label}</h3>
+            </Link>
+          )
+        })}
+      </div>
     </motion.div>
   );
 };
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -17,20 +17,20 @@ const SideBar = () => {
   const pathname = usePathname()
 
   return (
-    <div className=" border-r border-neutral-800">
-      <div className="flex pt-8 w-[200px] md:w-[250px] lg:w-[350px] flex-col py-4 gap-4    text-lg font-extralight ">
+    <div className="hidden border-r border-neutral-800">
+      <div className="flex pt-8  md:w-[250px] lg:w-[350px] flex-col py-4 gap-4    text-lg font-extralight ">
         {links.map((link) => {
           const isActive = pathname === link.href || pathname.startsWith(link.href + "/")
           return (
             <Link
               key={link.href}
               href={link.href}
-              className={`flex items-center gap-2 py-2 px-6 md:px-12 ${
+              className={`flex items-center gap-2 py-2 px-4 md:px-12 ${
                 isActive ? `bg-neutral-800/80 ` : "bg-transparent"
               }`}
             >
               <h3 className={`${link.color}`}>{link.icon}</h3>
-              <h3 className="">{link.label}</h3>
+              <h3 className="hidden lg:flex">{link.label}</h3>
             </Link>
           )
         })}
