7b223feaa2c0bbc45ea8c53b924f24768b09dbc1
Siddhesh <siddheshghag150@gmail.com>
2025-08-20 23:01:41 +0530

    fix: fixed config error

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-// next.config.js
 import type { NextConfig } from "next";
-import withPWA from 'next-pwa';
 
-// Create the PWA configuration
-const withPWAConfig = withPWA({
-  dest: 'public', // The folder where the service worker will be generated
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-  // More options can be added here
-});
-
-// Your original Next.js configuration
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
@@ -21,5 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-// Export the wrapped configuration
-export default withPWAConfig(nextConfig);
+export default nextConfig;
